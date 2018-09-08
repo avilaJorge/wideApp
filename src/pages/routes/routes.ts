04f6afb212ae49afe864dff1ff243644074f3005
Jorge Avila <avilajorge314@gmail.com>
2018-09-08 01:04:25 -0700

    Added a search bar for getting routes in particular areas.  Also added an infinite scroll feature for getting more the returned routes since we are only getting 20 at a time.  Finally added google-services.json to prepare for Firebase Cloud Messaging.

--- src/pages/routes/routes.ts
+++ src/pages/routes/routes.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
-import { IonicPage, LoadingController, NavController, NavParams } from 'ionic-angular';
+import { Component, NgZone } from '@angular/core';
+import { InfiniteScroll, IonicPage, LoadingController, NavController, NavParams } from 'ionic-angular';
+import { FormControl } from "@angular/forms";
+import { MapsAPILoader } from "@agm/core";
+import { } from 'googlemaps';
+
 import { RouteService } from "./routes.service";
 import { Route } from "./route.model";
 
@@ -17,19 +21,55 @@ import { Route } from "./route.model";
 })
 export class RoutesPage {
 
-  routes: Route[] = [];
-  lat: number = 32.87395225;
-  long: number = -117.22725327337258;
+  public routes: Route[] = [];
+  public lat: number = 32.87395225;
+  public long: number = -117.22725327337258;
+  public searchControl: FormControl;
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     private routeService: RouteService,
-    private loadingCtrl: LoadingController) {
+    private loadingCtrl: LoadingController,
+    private mapsAPILoader: MapsAPILoader,
+    private ngZone: NgZone)
+  {
+
+    // create search FormControl
+    this.searchControl = new FormControl();
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad RoutesPage');
+
+    // Load Places Autocomplete
+    this.mapsAPILoader.load().then(() => {
+      let elem = <HTMLInputElement>document.getElementsByClassName('searchbar-input')[0];
+      let autocomplete = new google.maps.places.Autocomplete(elem);
+      autocomplete.addListener("place_changed", () => {
+        this.ngZone.run(() => {
+          // get the place result
+          let place: google.maps.places.PlaceResult = autocomplete.getPlace();
+
+          // Verify result
+          if (place.geometry === undefined || place.geometry === null) {
+            return;
+          }
+
+          // set latitude, longitude
+          this.lat = place.geometry.location.lat();
+          this.long = place.geometry.location.lng();
+          this.updateRoutes();
+          console.log('Location found is ' + this.lat + ',' + this.long);
+        })
+      });
+    });
+
+    this.setCurrentPosition();
+    this.updateRoutes();
+  }
+
+  private updateRoutes() {
     let load = this.loadingCtrl.create({content: 'Loading Routes ...'});
     load.present();
     this.routeService.getRoutes(this.lat, this.long).then((routes) => {
@@ -55,6 +95,32 @@ export class RoutesPage {
       this.routes = routes;
       console.log(this.routes);
       load.dismiss();
+    });
+  }
+
+  getNewRoutes(ev: any) {
+    console.log(ev);
+    console.log(ev.target.value);
+  }
+
+  private setCurrentPosition() {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.lat = position.coords.latitude;
+        this.long = position.coords.longitude;
+        console.log('Location found is ' + this.lat + ',' + this.long);
+      });
+    }
+  }
+
+  getMoreRoutes(infiniteScroll: InfiniteScroll) {
+    this.routeService.getNextRoutes().then((routes) => {
+      infiniteScroll.complete();
+      if (routes) {
+        this.routes = routes;
+      } else {
+        infiniteScroll.enable(false);
+      }
     });
   }
 }
